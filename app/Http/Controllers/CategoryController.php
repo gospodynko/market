1b6e238d143fc280238fdca109a245c3187c0f8e
egorgrushko <egorgrushko@mail.ru>
2017-11-06 12:46:49 +0200

    Fix filter by categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,23 +1,24 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
     public function index($slug, $subSlug = false)
     {
         $per_page = 12;
-        $currSlug = $subSlug?: $slug;
-        $category = Category::with('parent')->where('slug', $currSlug)->first();
-        if(!$category) return abort(404);
-        $products = Product::where('category_id', $category->id)
-                            ->whereHas('user_products.shop', function ($q){
-                                $q->where('status', 1);
-                            });
-        return view('category', ['data' => ['products' => $products->paginate($per_page), 'cat_name' => $category->name],'category' => $category]);
+
+        if (!$subSlug) {
+            $category = Category::where('slug', $slug)->first();
+            $categories = $category->children()->orWhere('id', $category->id);
+        } else {
+            $categories = Category::where('slug', $subSlug);
+        }
+        $products = Product::whereIn('category_id', $categories->pluck('id'));
+
+        return view('category', ['data' => ['products' => $products->paginate($per_page), 'cat_name' => $category->name], 'category' => $category]);
     }
 }
