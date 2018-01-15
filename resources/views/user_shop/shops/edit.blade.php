648c13b60a46cea0d5b67ed7cfbdbcdb36fae1d2
defusandra <ab@uniphone.biz>
2018-01-15 17:18:03 +0200

    fix negative price

--- resources/views/user_shop/shops/edit.blade.php
+++ resources/views/user_shop/shops/edit.blade.php
@@ -5,29 +5,15 @@
         <div id="page-wrapper">
             <div class="container-fluid">
                 <div class="row">
-                    <h2 class="title">&nbsp;&nbsp;Добавьте новый продукт</h2>
-
-                    {{--<div v-if="checkedProduct" class="col-md-12 props-block">--}}
-                        {{--<div class="col-md-3">--}}
-                            {{--<!--<br/>-->--}}
-                            {{--<label class="mini-title category-title">Выбор категории:</label>--}}
-                            {{--<select disabled class="form-control category-form">--}}
-                                {{--<option :value="null" disabled selected>@{{ checkedProduct.category.name }}</option>--}}
-                            {{--</select>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-md-3">--}}
-                            {{--<label class="mini-title creator-title">Производитель:</label>--}}
-                            {{--<multiselect v-model="checkedTag" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="id" :options="checkedProducers" :multiple="false" :taggable="true" @tag="addTag"></multiselect>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-md-3 ">--}}
-                            {{--<label class="mini-title prod-title">Продукт:</label>--}}
-                            {{--<multiselect  v-model="checkedProduct" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="id" :options="checkedTag.hasOwnProperty('products') ? checkedTag.products : checkedProducts" :multiple="false" :taggable="true" @tag="addTagProduct"></multiselect>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-
+                    <h2 class="title">&nbsp;&nbsp;Редагування товару: </h2>
                     <div v-if="checkedProduct">
+                        <div class="col-md-3 ">
+                            <label class="mini-title prod-title">Продукт:</label>
+                            <textarea class="multiselect__input" v-model="checkedProduct.name" >@{{ checkedProduct.name }}</textarea>
+                         </div>
+                        </div>
                         <div class="col-lg-12 description-block">
-                            <label class="mini-title description-title">Описание продукта</label>
+                            <label class="mini-title description-title">Опис продукту</label>
                             <textarea v-model="checkedProduct.description" class="form-control description-form">@{{ checkedProduct.description }}</textarea>
 
                         </div>
@@ -36,12 +22,12 @@
                             <div v-for="feature in checkedProduct.features">
                                 <input type="text" placeholder="Заголовок характеристики" class="form-control head-param-title" v-model="feature.name">
                                 <div v-for="param in feature.params" class="param-block">
-                                    <input type="text" class="param-name" placeholder="Название параметра" v-model="param.title">
+                                    <input type="text" class="param-name" placeholder="Назва параметру" v-model="param.title">
                                     <input type="text" class="parametr" placeholder="Параметр" v-model="param.param">
                                 </div>
-                                <button class="btn btn-success add-param-btn" @click="addParameter(feature)"><i class="glyphicon glyphicon-plus"></i><span class="add-btn-text"> Добавить параметр</span></button>
+                                <button class="btn btn-success add-param-btn" @click="addParameter(feature)"><i class="glyphicon glyphicon-plus"></i><span class="add-btn-text"> Додати параметр</span></button>
                             </div>
-                            <button class="btn btn-success add-prop-btn" @click="addFeature"><i class="glyphicon glyphicon-plus"></i><span class="add-btn-text"> Добавить характеристику</span></button>
+                            <button class="btn btn-success add-prop-btn" @click="addFeature"><i class="glyphicon glyphicon-plus"></i><span class="add-btn-text"> Додати характеристику</span></button>
                         </div>
                         {{--<div class="col-md-12 load-file-block">--}}
                             {{--<div class="col-md-3">--}}
@@ -65,7 +51,7 @@
                     </div>
                     <div class="col-md-12 currency-block">
                         <div class="col-lg-3">
-                            <label class="mini-title price-title">Цена:</label>
+                            <label class="mini-title price-title">Ціна:</label>
                             <input type="number" class="form-control price-form" v-model="checkedProduct.price" value="@{{ checkedProduct.price }}" v-on:keypress="checkSymbol">
                         </div>
                         <div class="col-lg-3">
@@ -82,24 +68,24 @@
                     <div class="col-md-12 post-block">
                         <div class="col-lg-3">
                             <label class="mini-title post-title">Тип доставки:</label>
-                            <multiselect v-model="checkedProduct.delivery_types" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="id" :options="checkedProduct.delivery_type" :multiple="true"></multiselect>
+                            <multiselect v-model="checkedProduct.delivery_types" tag-placeholder="Додайте новий тег" placeholder="Пошук тегу" select-label="dsfjklghsdokfg awpsdfjoap" label="name" track-by="id" :options="checkedProduct.delivery_type" :multiple="true"></multiselect>
                         </div>
                         <div class="col-lg-3">
-                            <label class="mini-title pay-type-title">Тип оплаты:</label>
-                            <multiselect v-model="checkedProduct.pay_types" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="id" :options="checkedProduct.pay_type" :multiple="true"></multiselect>
+                            <label class="mini-title pay-type-title">Тип оплати:</label>
+                            <multiselect v-model="checkedProduct.pay_types" tag-placeholder="Додайте новий тег" placeholder="Пошук тегу" label="name" track-by="id" :options="checkedProduct.pay_type" :multiple="true"></multiselect>
                         </div>
                     </div>
                     <div class="col-md-12 btn-block">
                         <div class="col-lg-12 btn-lg-block">
                             <hr class="after-line">
-                            <a href="{{ route('user_shop.products') }}" class="btn btn-danger cancel-btn">
-                                <i class="glyphicon glyphicon-remove"></i>&nbsp;
-                                Отмена
-                            </a>
                             <button class="btn send-btn" @click="updateProduct">
                                 <i class="glyphicon glyphicon-send"></i>&nbsp;
-                                Редактировать
+                                Редагувати
                             </button>
+                            <a href="{{ route('user_shop.products') }}" class="btn btn-danger cancel-btn">
+                                <i class="glyphicon glyphicon-remove"></i>&nbsp;
+                                Відміна
+                            </a>
                         </div>
                     </div>
 
