83d970affaaeb4ba14a795f8a0480b45348c0a70
defusandra <ab@uniphone.biz>
2018-01-29 14:23:23 +0200

    added validation for edit form

--- resources/assets/js/componentsUserAdmin/shops/edit.vue
+++ resources/assets/js/componentsUserAdmin/shops/edit.vue
@@ -91,6 +91,14 @@
                     })
                 }
             },
+            checkSymbol (e){
+                var charCode = (e.which) ? e.which : e.keyCode
+                if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+                    e.preventDefault()
+                } else {
+                    return true
+                }
+            },
             deleteFeature (i) {
                 console.log(i);
                 this.product.features.splice(i,1);
