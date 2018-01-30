9665a62f7f60f934d4b32d70012379e3ce5a326e
defusandra <ad@uniphone.biz>
2018-01-30 11:32:16 +0200

    mc_main_480

--- resources/assets/js/components/core/header.vue
+++ resources/assets/js/components/core/header.vue
@@ -13,9 +13,49 @@
             <div class="two-wrap">
 
                 <div class="left">
+                    <svg class="svg-burger-menu-480" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 396.667 396.667" style="enable-background:new 0 0 396.667 396.667;" xml:space="preserve">
+<g>
+	<g>
+		<path d="M17,87.833h362.667c9.35,0,17-7.65,17-17s-7.65-17-17-17H17c-9.35,0-17,7.65-17,17C0,80.183,7.65,87.833,17,87.833z"/>
+		<path d="M17,215.333h362.667c9.35,0,17-7.65,17-17s-7.65-17-17-17H17c-9.35,0-17,7.65-17,17S7.65,215.333,17,215.333z"/>
+		<path d="M17,342.833h362.667c9.35,0,17-7.65,17-17s-7.65-17-17-17H17c-9.35,0-17,7.65-17,17S7.65,342.833,17,342.833z"/>
+	</g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
                     <div class="logo">
                         <a href="/">
-                            <img src="/img/header/logo.svg" alt="">
+                           <img class="img-logo-480" src="/img/header/logo_480.svg" alt="">
+                           <img class="img-logo-1024" src="/img/header/logo.svg" alt="">
                         </a>
                     </div>
                 </div>
@@ -28,15 +68,28 @@
                             <a href="/shop/products" class="shop" v-if="user && user.role == 'seller'"><i></i> {{translate.my_shop}}</a>
                         </div>
                         <div class="item">
-                            <a href="#" class="basket" @click="showCartFunc"><i></i><span class="badge" :class="{'badge': cart}" v-if="cart && cart.length">{{cart.length}}</span> {{translate.cart}}</a>
+                            <a href="#" class="basket" @click="showCartFunc">
+                                <i></i>
+                                <span class="badge" :class="{'badge': cart}" v-if="cart && cart.length">{{cart.length}}</span>
+                                <p>
+                                    {{translate.cart}}
+                                </p>
+                            </a>
                         </div>
                         <div class="item">
                             <div class="user-wrap" v-if="user">
                                 <img src="/img/avatars/ava.png" alt="" class="avatar">
                                 <span class="logout" @click="logout">{{translate.exit}}</span>
                             </div>
-
-                            <a href="/login" v-else>{{translate.enter}}</a>
+                            <a href="/login" v-else>
+                                <svg class="svg-login-480" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 258.8 258.8" style="enable-background:new 0 0 258.8 258.8;" xml:space="preserve">
+<circle cx="129.4" cy="109" r="60"/>
+<path d="M129.4,181c-60.1,0-108.8,34.8-108.8,77.8h217.5C238.1,215.8,189.4,181,129.4,181z"/>
+</svg>
+                                <p>
+                                    {{translate.enter}}
+                                </p>
+                            </a>
                         </div>
                         <!--<div class="item">-->
                         <!--&lt;!&ndash;<ul class="languages">&ndash;&gt;-->
@@ -89,7 +142,44 @@
                             <form method="get" action="/search" ref="custom" v-on:submit.prevent="onSubmit">
                                 <label for="search-input">
                                     <input type="text" id="search-input" name="q" v-model="q">
-                                    <button type="submit" class="btn">{{translate.search}}</button>
+                                    <button type="submit" class="btn">
+                                        <svg class="svg-search-btn" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve">
+<path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
+                                        <p>
+                                            {{translate.search}}
+                                        </p>
+                                    </button>
                                 </label>
                             </form>
                         </div>
