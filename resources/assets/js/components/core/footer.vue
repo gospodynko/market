fb4b7105dcf6f10b41f72255109497bc2ae7cb2b
defusandra <ad@uniphone.biz>
2018-01-30 17:50:53 +0200

    mc_main_page_20180130

--- resources/assets/js/components/core/footer.vue
+++ resources/assets/js/components/core/footer.vue
@@ -1,58 +1,131 @@
 <template>
     <footer class="footer-section">
-        <div class="head-footer-wrap">
-            <div class="site-naw-wrap">
-                <ul class="head-footer-menu">
-                    <li>{{translate.company}}</li>
-                    <li>{{translate.tool}}</li>
-                    <li>{{translate.contacts}}</li>
-                    <li>{{translate.address}}</li>
-                </ul>
-            </div>
-        </div>
-        <div class="main-footer-wrap">
-            <div class="all-items-menu-wrap">
-                <div class="single-menu-item">
-                    <ul class="list-company-info">
-                        <li><a href="https://agroyard.ua/news">{{translate.news}}</a></li>
-                        <li><a href="https://agroyard.ua/about">{{translate.about}}</a></li>
-                        <li><a href="https://agroyard.ua/callback">{{translate.feedback}}</a></li>
+        <div class="head-footer-wrap-bigScreen">
+            <div class="head-footer-wrap">
+                <div class="site-naw-wrap">
+                    <ul class="head-footer-menu">
+                        <li>{{translate.company}}</li>
+                        <li>{{translate.tool}}</li>
+                        <li>{{translate.contacts}}</li>
+                        <li>{{translate.address}}</li>
                     </ul>
-                </div>
-                <div class="single-menu-item">
-                    <ul class="list-company-info">
-                        <li><a href="https://agroyard.ua/auctions/all">{{translate.sell_crop}}</a></li>
-                        <li><a href="https://agroyard.ua/classifieds/gsm?id=1">{{translate.adv}}</a></li>
-                        <li><a href="https://agroyard.ua/auctions/all">{{translate.auction}}</a></li>
-                        <!--<li><a href="#">{{translate.tenders}}</a></li>-->
-                    </ul>
-                </div>
-                <div class="single-menu-item">
-                    <ul class="list-company-info">
-                        <li><a href="tel:0800802141">(0800) 80 21 41</a></li>
-                        <li><a href="tel:0501992141">(050) 199 21 41</a></li>
-                        <li><a href="tel:0961992141">(067) 199 21 41</a></li>
-                        <li><a href="tel:0931992141">(093) 199 21 41</a></li>
-                        <li><a href="mailto:info@agroyard.com.ua">info@agroyard.ua</a></li>
-                    </ul>
-                </div>
-                <div class="single-menu-item">
-                    <ul class="list-company-info">
-                        <li>{{translate.detail_address_1}}</li>
-                        <li>{{translate.detail_address_2}}</li>
-                    </ul>
-                </div>
-                <div class="qr-wrap">
                 </div>
             </div>
-        </div>
-        <div class="bottom-footer-detail">
-            <div class="all-det-wrap">
-                <div class="left">
-                    <p>&#169; АгроЯрд 2017 г. {{translate.all_right}}</p>
+            <div class="main-footer-wrap">
+                <div class="all-items-menu-wrap">
+                    <div class="single-menu-item">
+                        <ul class="list-company-info">
+                            <li><a href="https://agroyard.ua/news">{{translate.news}}</a></li>
+                            <li><a href="https://agroyard.ua/about">{{translate.about}}</a></li>
+                            <li><a href="https://agroyard.ua/callback">{{translate.feedback}}</a></li>
+                        </ul>
+                    </div>
+                    <div class="single-menu-item">
+                        <ul class="list-company-info">
+                            <li><a href="https://agroyard.ua/auctions/all">{{translate.sell_crop}}</a></li>
+                            <li><a href="https://agroyard.ua/classifieds/gsm?id=1">{{translate.adv}}</a></li>
+                            <li><a href="https://agroyard.ua/auctions/all">{{translate.auction}}</a></li>
+                            <!--<li><a href="#">{{translate.tenders}}</a></li>-->
+                        </ul>
+                    </div>
+                    <div class="single-menu-item">
+                        <ul class="list-company-info">
+                            <li><a href="tel:0800802141">(0800) 80 21 41</a></li>
+                            <li><a href="tel:0501992141">(050) 199 21 41</a></li>
+                            <li><a href="tel:0961992141">(067) 199 21 41</a></li>
+                            <li><a href="tel:0931992141">(093) 199 21 41</a></li>
+                            <li><a href="mailto:info@agroyard.com.ua">info@agroyard.ua</a></li>
+                        </ul>
+                    </div>
+                    <div class="single-menu-item">
+                        <ul class="list-company-info">
+                            <li>{{translate.detail_address_1}}</li>
+                            <li>{{translate.detail_address_2}}</li>
+                        </ul>
+                    </div>
+                    <div class="qr-wrap">
+                    </div>
                 </div>
-                <div class="right">
-                    <p>{{translate.site_create}}</p>
+            </div>
+            <div class="bottom-footer-detail">
+                <div class="all-det-wrap">
+                    <div class="left">
+                        <p>&#169; АгроЯрд 2017 г. {{translate.all_right}}</p>
+                    </div>
+                    <div class="right">
+                        <p>{{translate.site_create}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer-mobile -->
+        <div class="head-footer-wrap-mobile">
+            <div class="head-footer-wrap">
+                <div class="site-naw-wrap">
+                    <ul class="head-footer-menu">
+                        <li>{{translate.company}}</li>
+                        <li>{{translate.tool}}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="main-footer-wrap">
+                <div class="all-items-menu-wrap">
+                    <div class="single-menu-item">
+                        <ul class="list-company-info">
+                            <li><a href="https://agroyard.ua/news">{{translate.news}}</a></li>
+                            <li><a href="https://agroyard.ua/about">{{translate.about}}</a></li>
+                            <li><a href="https://agroyard.ua/callback">{{translate.feedback}}</a></li>
+                        </ul>
+                    </div>
+                    <div class="single-menu-item">
+                        <ul class="list-company-info">
+                            <li><a href="https://agroyard.ua/auctions/all">{{translate.sell_crop}}</a></li>
+                            <li><a href="https://agroyard.ua/classifieds/gsm?id=1">{{translate.adv}}</a></li>
+                            <li><a href="https://agroyard.ua/auctions/all">{{translate.auction}}</a></li>
+                            <!--<li><a href="#">{{translate.tenders}}</a></li>-->
+                        </ul>
+                    </div>
+                    <div class="single-menu-item contacts-480">
+                        <p>{{translate.contacts}}</p>
+                        <ul class="list-company-info">
+                            <li><a href="tel:0800802141">(0800) 80 21 41</a></li>
+                            <li><a href="tel:0501992141">(050) 199 21 41</a></li>
+                            <li><a href="tel:0961992141">(067) 199 21 41</a></li>
+                            <li><a href="tel:0931992141">(093) 199 21 41</a></li>
+                            <li><a href="mailto:info@agroyard.com.ua">info@agroyard.ua</a></li>
+                        </ul>
+                    </div>
+                    <div class="single-menu-item address-480">
+                        <!--<p>{{translate.address}}</p>-->
+                        <img class="img-geoLoc-480" src="/img/footer/geo_loc_480.png" alt="">
+                        <ul class="list-company-info">
+                            <li>{{translate.detail_address_1}}</li>
+                            <li>{{translate.detail_address_2}}</li>
+                        </ul>
+                    </div>
+                    <div class="qr-wrap">
+                    </div>
+                </div>
+            </div>
+            <div class="social-network">
+                <p> Соцсети</p>
+                <div class="flex-social-network">
+                    <a href="https://www.facebook.com/agroyard/">
+                        <img class="img-fb-social-network" src="/img/footer/fb.png" alt="">
+                    </a>
+                    <!--<a href="/">-->
+                        <!--<img class="img-vk-social-network" src="/img/footer/vk.png" alt="">-->
+                    <!--</a>-->
+                </div>
+            </div>
+            <div class="bottom-footer-detail">
+                <div class="all-det-wrap">
+                    <div class="left">
+                        <p>&#169; АгроЯрд 2017 г. {{translate.all_right}}</p>
+                    </div>
+                    <div class="right">
+                        <p>{{translate.site_create}}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/assets/js/components/index/top-product.vue
+++ resources/assets/js/components/index/top-product.vue
@@ -1,6 +1,11 @@
 <template>
     <section class="top-product">
         <div class="top-product-wrap">
+            <div class="best-price-480">
+                <p>Посевной материал в Украине</p>
+                <p>Лучшие цены</p>
+                <p class="price-best-price-480">от 5 000 до 7 000 грн</p>
+            </div>
             <h2>{{translate.top_items}}</h2>
             <div class="all-products-list">
                 <div class="single-product" v-for="product in allProducts.data">
@@ -12,10 +17,35 @@
                             <a :href="product.url">{{product.name}}</a>
                         </p>
                         <p class="price">
-                            {{numberWithSpaces(product.price)}} {{product.currency.name}}
+                            {{numberWithSpaces(product.price)}} грн <!-- {{product.currency.name}} -->
                         </p>
                     </div>
+                    <div class="hide-list-wrap-480">
+                        <div class="all-goods-btn">
+                            <a :href="product.url" class="btn">
+                                Детальніше
+                            </a>
+                        </div>
+                        <div class="two-wrap" v-if="false">
+                            <div class="left">
+                                <i></i>
+                                <span>{{translate.spy_good}}</span>
+                            </div>
+                            <div class="right">
+                                <i></i>
+                                <span>{{translate.faworite}}</span>
+                            </div>
+                        </div>
+                    </div>
                     <div class="detail-prod-wrap">
+                        <div class="feedback-wrap-480">
+                            <div class="rating-wrap">
+                                <star-rating :star-size="15" :increment="0.01" :rating=product.rate :read-only="true" :show-rating="true"></star-rating>
+                            </div>
+                            <div class="count-feedback-wrap">
+                                <a href="#">{{product.reviews.length}} {{translate.reviews}}</a>
+                            </div>
+                        </div>
                         <div class="feedback-wrap">
                             <div class="rating-wrap">
                                 <star-rating :star-size="20" :increment="0.01" :rating=product.rate :read-only="true" :show-rating="true"></star-rating>
@@ -54,7 +84,6 @@
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
                 <div class="show-all-btn-wrap" v-if="products && allProducts.total > 12">
