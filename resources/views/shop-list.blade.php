b1bc9cae0d2507b3422670c81265a09b6fd5c3ac
defusandra <ab@uniphone.biz>
2018-02-02 15:24:08 +0200

    categories

--- resources/views/shop-list.blade.php
+++ resources/views/shop-list.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.index')
 @section('content')
-    <all-shops-vue :shop-list="{{json_encode($shop_list)}}" :breadcrumbs="{}"></all-shops-vue>
+    <all-shops-vue :shop-list="{{json_encode($shop_list)}}" :categories="{{json_encode($categories)}}" :breadcrumbs="{}"></all-shops-vue>
 @endsection
