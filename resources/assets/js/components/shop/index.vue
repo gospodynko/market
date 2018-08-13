c96fa86a08aa3b360fd52d2d163822c15677902c
Oleh <oi@uniphone.biz>
2018-08-13 14:56:50 +0300

    fix

--- resources/assets/js/components/shop/index.vue
+++ resources/assets/js/components/shop/index.vue
@@ -173,8 +173,8 @@
                                     <a :href="'/' + product.url">{{product.name}}</a>
                                 </p>
                                 <p class="price">
-                                    {{numberWithSpaces(product.price)}} грн
-                                    <!--{{product.currency.name}}.-->
+                                    {{numberWithSpaces(product.price)}}
+                                    {{product.currency.name}}
                                 </p>
                             </div>
                             <div class="detail-prod-wrap">
